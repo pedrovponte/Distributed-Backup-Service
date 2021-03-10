3fe70e7b5e8e94025a73a01b6b423da078a999dd
Pedro <up201809694@fe.up.pt>
2021-03-10 23:48:40 +0000

    Implemented client and started peer

---
+++ proj1/src/Client.java
@@ -0,0 +1,100 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            if(args.length > 4) {
+                System.out.println("Usage: Client <peer_ap> <sub_protocol> [<opnd_1> [<opnd_2>]]");
+            }
+
+            String peer_ap = args[0];
+            String subprotocol = args[1].toUpperCase();
+            String file_path_name;
+            int replication_degree;
+            int maximum_disk_space;
+
+            Registry registry = LocateRegistry.getRegistry("localhost"); // deve ser dado nos argumentos?
+            RemoteInterface stub = (RemoteInterface) registry.lookup(peer_ap);
+
+            switch (subprotocol) {
+                case "BACKUP":
+                    if(args.length != 4) {
+                        System.out.println("Invalid invocation for BACKUP subprotocol.");
+                        System.out.println("Usage: Client <peer_ap> BACKUP <file_path_name> <replication_degree>");
+                        return;
+                    }
+
+                    file_path_name = args[2];
+                    replication_degree = Integer.parseInt(args[3]);
+                    System.out.println("Peer_ap: " + peer_ap);
+                    System.out.println("Subprotocol: " + subprotocol);
+                    System.out.println("File path: " + file_path_name);
+                    System.out.println("Replication: " + replication_degree);
+                    stub.backup(file_path_name, replication_degree);
+                    break;
+                
+                case "RESTORE":
+                    if(args.length != 3) {
+                        System.out.println("Invalid invocation for RESTORE subprotocol.");
+                        System.out.println("Usage: Client <peer_ap> RESTORE <file_path_name>");
+                        return;
+                    }
+
+                    file_path_name = args[2];
+                    System.out.println("Peer_ap: " + peer_ap);
+                    System.out.println("Subprotocol: " + subprotocol);
+                    System.out.println("File path: " + file_path_name);
+                    stub.restore(file_path_name);
+                    break;
+
+                case "DELETE":
+                    if(args.length != 3) {
+                        System.out.println("Invalid invocation for DELETE subprotocol.");
+                        System.out.println("Usage: Client <peer_ap> DELETE <file_path_name>");
+                        return;
+                    }
+
+                    file_path_name = args[2];
+                    System.out.println("Peer_ap: " + peer_ap);
+                    System.out.println("Subprotocol: " + subprotocol);
+                    System.out.println("File path: " + file_path_name);
+                    stub.restore(file_path_name);
+                    break;
+
+                case "RECLAIM":
+                    if(args.length != 3) {
+                        System.out.println("Invalid invocation for RECLAIM subprotocol.");
+                        System.out.println("Usage: Client <peer_ap> RECLAIM <maximum_disk_space>");
+                        return;
+                    }
+
+                    maximum_disk_space = Integer.parseInt(args[2]);
+                    System.out.println("Peer_ap: " + peer_ap);
+                    System.out.println("Subprotocol: " + subprotocol);
+                    System.out.println("Maximum disk: " + maximum_disk_space);
+                    stub.reclaim(maximum_disk_space);
+                    break;
+
+                case "STATE":
+                    if(args.length != 2) {
+                        System.out.println("Invalid invocation for STATE subprotocol.");
+                        System.out.println("Usage: Client <peer_ap> STATE");
+                        return;
+                    }
+
+                    System.out.println("Peer_ap: " + peer_ap);
+                    System.out.println("Subprotocol: " + subprotocol);
+                    stub.state();
+                    break;
+
+                default:
+                    break;
+            }
+
+        } catch(Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ proj1/src/Peer.java
@@ -0,0 +1,31 @@
+public class Peer implements RemoteInterface {
+
+    public Peer() {
+    }
+    
+    @Override
+    public void backup(String path, int replication) {
+
+    }
+
+    @Override
+    public void restore(String path) {
+
+    }
+
+    @Override
+    public void delete(String path) {
+
+    }
+    
+    @Override
+    public void reclaim(int maximum_disk_space) {
+
+    }
+
+    @Override
+    public void state() {
+
+    }
+    
+}
